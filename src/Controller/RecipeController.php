cd4e3786314b16cc57982c8a74285e60b1cf8760
Axel Vair <axelvair@icloud.com>
2023-08-06 13:35:45 +0200

    :sparkles: receipts page

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\RecipeRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RecipeController extends AbstractController
 {
-    #[Route('/recipe', name: 'app_recipe')]
-    public function index(): Response
+    #[Route('/recipe', 'recipe.index')]
+    public function index(RecipeRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
-        return $this->render('recipe/index.html.twig', [
-            'controller_name' => 'RecipeController',
+        $receipts = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1),
+        10
+        );
+        return $this->render('pages/recipe/index.html.twig', [
+            'receipts' => $receipts
         ]);
     }
 }
