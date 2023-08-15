8d10d8842180a7b05b7d031456a142a4ab8547eb
Axel Vair <contact@axelvair.fr>
2023-08-15 17:22:08 +0200

    :sparkles: add delete feature for recipes

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -17,8 +17,8 @@ class RecipeController extends AbstractController
 {
     #[Route('/recette', name: 'recipe.index', methods: ['GET'])]
     public function index(PaginatorInterface $paginator,
-                          RecipeRepository $repository,
-                          Request $request
+                          RecipeRepository   $repository,
+                          Request            $request
     ): Response
     {
         $recipes = $paginator->paginate(
@@ -27,13 +27,13 @@ class RecipeController extends AbstractController
             10 /*limit per page*/
         );
         return $this->render('pages/recipe/index.html.twig', [
-           'recipes' => $recipes
+            'recipes' => $recipes
         ]);
     }
 
-    #[Route('/recette/creation', 'recipe.new', methods: ['GET','POST'])]
+    #[Route('/recette/creation', 'recipe.new', methods: ['GET', 'POST'])]
     public function new(
-        Request $request,
+        Request                $request,
         EntityManagerInterface $manager
     ): Response
     {
@@ -42,7 +42,7 @@ class RecipeController extends AbstractController
 
 
         $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $recipe = $form->getData();
             $manager->persist($recipe);
             $manager->flush();
@@ -58,7 +58,61 @@ class RecipeController extends AbstractController
         return $this->render('pages/recipe/new.html.twig', [
             'form' => $form->createView()
         ]);
-
     }
 
+    #[Route('/recette/edition/{id}', 'recipe.edit', methods: ['GET', 'POST'])]
+    public function edit(RecipeRepository $repository,
+                         int $id, Request $request,
+                         EntityManagerInterface $manager
+    ): Response
+    {
+        $recipe = $repository->findOneBy(["id" => $id]);
+        $form = $this->createForm(RecipeType::class, $recipe);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $recipe = $form->getData();
+
+            $manager->persist($recipe);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre recette a bien été modifiée ! '
+            );
+            return $this->redirectToRoute('recipe.index');
+        }
+
+        return $this->render('pages/recipe/edit.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/recette/suppression/{id}', 'recipe.delete', methods: ['GET'])]
+    public function delete(RecipeRepository $repository,
+                           int $id,
+                           EntityManagerInterface $manager) : Response
+    {
+        if(!$id){
+
+            $this->addFlash(
+                'success',
+                'La recette n\'a pas été trouvé !'
+            );
+
+            return $this->redirectToRoute('recipe.index');
+        }
+
+
+        $recipe = $repository->findOneBy(["id" => $id]);
+        $manager->remove($recipe);
+        $manager->flush();
+
+        $this->addFlash(
+            'success',
+            'Votre recette a bien été supprimée !'
+        );
+
+        return $this->redirectToRoute('recipe.index');
+    }
 }
--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -124,12 +124,12 @@ class RecipeType extends AbstractType
             ->add('isFavorite', CheckboxType::class, [
 
                 'attr' => [
-                    'class' => 'form-control',
+                    'class' => 'form-check-input',
                     ],
                     'required' => false,
                     'label' => 'Favoris ?',
                     'label_attr' => [
-                        'class' => 'form-label mt-4'
+                        'class' => 'form-check-label'
                 ],
                 'constraints' =>
                     new Assert\NotNull()
